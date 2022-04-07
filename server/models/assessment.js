8358a54b12afdcf5c03003b5f86c62d250db20a3
kaitlinchoffmann <kaitlinchoffmann@gmail.com>
2022-04-07 00:05:39 -0400

    Initial creation of app

---
+++ server/models/assessment.js
@@ -0,0 +1,38 @@
+const assessment = [
+  {
+    assessmentId: 43233,
+    userId: 12345,
+    bmi: 20,
+  },
+  {
+    assessmentId: 22222,
+    userId: 55555,
+    bmi: 30,
+  },
+  {
+    assessmentId: 93999,
+    userId: 34212,
+    bmi: 17,
+  }
+]
+
+let getHealthState = (userId) => {
+  let bmi = getBMI(userId);
+  return calculateHealth(bmi)
+};
+
+function calculateHealth(bmi) {
+  let healthState;
+  if(bmi < 18.5) {
+    healthState = "underweight";
+  } else if(bmi <= 25){
+    healthState = "normal";
+  } else if(bmi <= 30) {
+    healthState = "overweight";
+  } else {
+    healthState = "obese";
+  }
+  return healthState;
+}
+
+module.exports = { };
